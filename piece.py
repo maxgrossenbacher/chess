ffdba342051a5b256fcbbb3fd0a01229721559b1
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-11-01 12:09:24 -0600

    update piece class

--- piece.py
+++ piece.py
@@ -5,11 +5,21 @@ class Pawn():
     def __init__(self, color, location, value=1):
         self.color = color
         self.value = value
-        self.location = list(location)
-        self.col = int(location[0])
-        self.row = int(location[1])
+        self.location = location
+        self.col = location[0]
+        self.row = location[1]
         self.moves = []
         self.valid_moves = []
+        self.can_capture = False
+
+    def move(self, move):
+        self.valid_moves = []
+        self._possible_moves()
+        if move in self.valid_moves:
+            self.moves.append(move)
+            self._update_location(move)
+        else:
+            print('Sorry, not a valid move...try again')
 
     def __str__(self):
         if self.color == 'white':
@@ -17,42 +27,115 @@ class Pawn():
         if self.color == 'black':
             return '\u2659'
 
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
+
+    def _possible_moves(self):
+        if self.color == 'white':
+            self.valid_moves.append((self.col, (self.row - 1)))
+            if len(self.moves) == 0:
+                self.valid_moves.append((self.col, (self.row - 2)))
+            if can_capture == True:
+                self.valid_moves.append(((self.col + 1), (self.row - 1)))
+                self.valid_moves.append(((self.col - 1), (self.row - 1)))
+        elif self.color == 'black':
+            self.valid_moves.append((self.col, (self.row + 1)))
+            if len(self.moves) == 0:
+                self.valid_moves.append((self.col, (self.row + 2)))
+            if self.can_capture == True:
+                self.valid_moves.append(((self.col - 1), (self.row + 1)))
+                self.valid_moves.append(((self.col + 1), (self.row + 1)))
+
+
+
+class Bishop():
+    def __init__(self, color, location, value=3):
+        self.color = color
+        self.value = value
+        self.location = location
+        self.col = location[0]
+        self.row = location[1]
+        self.moves = []
+        self.valid_moves = []
+        self.can_capture = False
+
     def move(self, move):
         self.valid_moves = []
-        self._capture_moves()
-        self._non_capture_moves()
-        if self._check_valid_move(move) == 'valid_move':
+        self._possible_moves()
+        if move in self.valid_moves:
             self.moves.append(move)
-            self.location = move
-            self.col = move[0]
-            self.row = move[1]
+            self._update_location(move)
         else:
             print('Sorry, not a valid move...try again')
 
-    def _non_capture_moves(self):
+    def __str__(self):
         if self.color == 'white':
-            self.valid_moves.append([self.col, (self.row + 1)])
-            if len(self.moves) == 0:
-                self.valid_moves.append([self.col, (self.row + 2)])
+            return '\u265D'
         if self.color == 'black':
-            self.valid_moves.append([self.col, (self.row - 1)])
-            if len(self.moves) == 0:
-                self.valid_moves.append([self.col, (self.row - 2)])
+            return '\u2657'
 
-    def _capture_moves(self):
-        if self.color == 'white':
-            self.valid_moves.append([(self.col + 1), (self.row + 1)])
-            self.valid_moves.append([(self.col - 1), (self.row + 1)])
-        if self.color == 'black':
-            self.valid_moves.append([str(self.col - 1), (self.row - 1)])
-            self.valid_moves.append([(self.col + 1), (self.row - 1)])
-        return
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
 
-    def _check_valid_move(self, move):
+    def _possible_moves(self):
+        count = -7
+        for _ in range(14):
+            self.rl_row = self.row + count
+            self.rl_col = self.col + count
+            self.lr_row = self.row + count
+            self.lr_col = self.col - count
+            if self.rl_row >= 0 and self.rl_row <= 7 and self.rl_row != self.row and self.rl_col >= 0 and self.rl_col <=7 and self.rl_col != self.col:
+                self.valid_moves.append((self.rl_col, self.rl_row))
+            if self.lr_row >= 0 and self.lr_row <= 7 and self.lr_row != self.row and self.lr_col >= 0 and self.lr_col <=7 and self.lr_col != self.col:
+                self.valid_moves.append((self.lr_col, self.lr_row))
+            count+=1
+
+
+
+class Knight():
+    def __init__(self, color, location, value=3):
+        self.color = color
+        self.value = value
+        self.location = location
+        self.col = location[0]
+        self.row = location[1]
+        self.moves = []
+        self.valid_moves = []
+        self.can_capture = False
+
+    def move(self, move):
+        self.valid_moves = []
+        self._possible_moves()
         if move in self.valid_moves:
-            return 'valid_move'
+            self.moves.append(move)
+            self._update_location(move)
         else:
-            return 'invalid move'
+            print('Sorry, not a valid move...try again')
+
+    def __str__(self):
+        if self.color == 'white':
+            return '\u265E'
+        if self.color == 'black':
+            return '\u2658'
+
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
+
+    def _possible_moves(self):
+        possible_moves = [((self.col - 1), (self.row + 2)), ((self.col + 1), (self.row + 2)),\
+                            ((self.col + 2), (self.row + 1)), ((self.col + 2), (self.row - 1)),\
+                            ((self.col + 1), (self.row - 2)), ((self.col - 1), (self.row - 2)),\
+                            ((self.col - 2), (self.row - 1)), ((self.col - 2), (self.row + 1))]
+        for pm in possible_moves:
+            c, r = pm
+            if r >= 0 and r <= 7 and c >= 0 and c <=7:
+                self.valid_moves.append((c, r))
 
 
 
@@ -61,20 +144,19 @@ class Rook():
         self.color = color
         self.value = value
         self.location = location
-        self.col = self.location[0]
-        self.row = self.location[1]
+        self.col = location[0]
+        self.row = location[1]
         self.moves = []
-        self.castle = True
         self.valid_moves = []
+        self.can_capture = False
+        self.castle = True
 
     def move(self, move):
         self.valid_moves = []
         self._possible_moves()
-        if self._check_valid_move(move) == 'valid_move':
+        if move in self.valid_moves:
             self.moves.append(move)
-            self.location = move
-            self.col = move[0]
-            self.row = move[1]
+            self._update_location(move)
         else:
             print('Sorry, not a valid move...try again')
 
@@ -84,51 +166,54 @@ class Rook():
         if self.color == 'black':
             return '\u2656'
 
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
+
     def _possible_moves(self):
         if len(self.moves) == 0:
             self.castle = True
-        count = 1
-        for _ in range(7):
-            self.valid_moves.append([self.col, count])
-            self.valid_moves.append([count, self.row])
+        count = 0
+        for _ in range(8):
+            if (self.col, count) != self.location:
+                self.valid_moves.append((self.col, count))
+            if (count, self.row) != self.location:
+                self.valid_moves.append((count, self.row))
             count+=1
 
-    def _check_valid_move(self, move):
-        if move in self.valid_moves:
-            return 'valid_move'
-        else:
-            return 'invalid move'
 
 
-
-class Bishop():
-    def __init__(self, color, location, value=5):
+class Queen():
+    def __init__(self, color, location, value=9):
         self.color = color
         self.value = value
         self.location = location
-        self.col = self.location[0]
-        self.row = self.location[1]
+        self.col = location[0]
+        self.row = location[1]
         self.moves = []
-        self.castle = True
         self.valid_moves = []
+        self.can_capture = False
 
     def move(self, move):
         self.valid_moves = []
         self._possible_moves()
-        if self._check_valid_move(move) == 'valid_move':
+        if move in self.valid_moves:
             self.moves.append(move)
-            self.location = move
-            self.col = move[0]
-            self.row = move[1]
+            self._update_location(move)
         else:
             print('Sorry, not a valid move...try again')
-
 
     def __str__(self):
         if self.color == 'white':
             return '\u265D'
         if self.color == 'black':
             return '\u2657'
+
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
 
     def _possible_moves(self):
         count = -7
@@ -137,14 +222,60 @@ class Bishop():
             self.rl_col = self.col + count
             self.lr_row = self.row + count
             self.lr_col = self.col - count
-            if self.rl_row >= 0 and self.rl_row <= 7 and self.rl_col >= 0 and self.rl_col <=7:
-                self.valid_moves.append([self.rl_col, self.rl_row])
-            if self.lr_row >= 0 and self.lr_row <= 7 and self.lr_col >= 0 and self.lr_col <=7:
-                self.valid_moves.append([self.lr_col, self.lr_row])
+            if self.rl_row >= 0 and self.rl_row <= 7 and self.rl_row != self.row and self.rl_col >= 0 and self.rl_col <=7 and self.rl_col != self.col:
+                self.valid_moves.append((self.rl_col, self.rl_row))
+            if self.lr_row >= 0 and self.lr_row <= 7 and self.lr_row != self.row and self.lr_col >= 0 and self.lr_col <=7 and self.lr_col != self.col:
+                self.valid_moves.append((self.lr_col, self.lr_row))
+            count+=1
+        count = 0
+        for _ in range(8):
+            if (self.col, count) != self.location:
+                self.valid_moves.append((self.col, count))
+            if (count, self.row) != self.location:
+                self.valid_moves.append((count, self.row))
             count+=1
 
-    def _check_valid_move(self, move):
+
+class King():
+    def __init__(self, color, location, value=3):
+        self.color = color
+        self.value = value
+        self.location = location
+        self.col = location[0]
+        self.row = location[1]
+        self.moves = []
+        self.valid_moves = []
+        self.can_capture = False
+        self.castle = True
+
+    def move(self, move):
+        self.valid_moves = []
+        self._possible_moves()
         if move in self.valid_moves:
-            return 'valid_move'
+            self.moves.append(move)
+            self._update_location(move)
         else:
-            return 'invalid move'
+            print('Sorry, not a valid move...try again')
+
+    def __str__(self):
+        if self.color == 'white':
+            return '\u265E'
+        if self.color == 'black':
+            return '\u2658'
+
+    def _update_location(self, newlocal):
+        self.location = newlocal
+        self.col = newlocal[0]
+        self.row = newlocal[1]
+
+    def _possible_moves(self):
+        if len(self.moves) == 0:
+            self.castle = True
+        possible_moves = [((self.col - 1), (self.row - 1)), ((self.col), (self.row - 1)),\
+                            ((self.col + 1), (self.row - 1)), ((self.col + 1), (self.row)),\
+                            ((self.col + 1), (self.row + 1)), ((self.col), (self.row + 1)),\
+                            ((self.col - 1), (self.row + 1)), ((self.col - 1), (self.row))]
+        for pm in possible_moves:
+            c, r = pm
+            if r >= 0 and r <= 7 and c >= 0 and c <=7:
+                self.valid_moves.append((c, r))
